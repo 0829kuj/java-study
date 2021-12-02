da2994d8f04b84417544e272e781b6c8eaa8d22b
0829kuj <0829kuj@naver.com>
2021-12-02 10:54:14 +0900

    211202오오전2

---
+++ Ch15_Swing2/src/references/App.java
@@ -0,0 +1,19 @@
+package references;
+
+class Person {
+	
+}
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person p1 = new Person();
+		System.out.println(p1);
+		
+		Person p2 = p1;
+		
+		System.out.println(p2);
+	}
+
+}
