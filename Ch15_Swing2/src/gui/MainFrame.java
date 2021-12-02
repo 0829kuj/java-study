cf2a742a69af9b71edaff92d2908a38671ab1ae9
0829kuj <0829kuj@naver.com>
2021-12-02 11:27:06 +0900

    211202오전3

--- Ch15_Swing2/src/gui/MainFrame.java
+++ Ch15_Swing2/src/gui/MainFrame.java
@@ -14,7 +14,7 @@ public class MainFrame extends JFrame {
 		final MainPanel mainPanel = new MainPanel();
 		setLayout(new BorderLayout());	// 창에 컴포넌트를 붙이기 위한 레이아웃
 		
-		add(new Toolbar(), BorderLayout.NORTH);	// 위쪽에 툴바 붙이기
+		add(new Toolbar(mainPanel), BorderLayout.NORTH);	// 위쪽에 툴바 붙이기
 		add(mainPanel, BorderLayout.CENTER);	// 위에서 만든 패널을 레이아웃의 가운데 붙이기
 		
 		setSize(600, 400);
--- Ch15_Swing2/src/gui/Toolbar.java
+++ Ch15_Swing2/src/gui/Toolbar.java
@@ -1,17 +1,39 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JToolBar;
 
-class redButtonListener {
+class ColorButtonListener implements ActionListener {
+	private MainPanel mainPanel;
+	private Color c;
 	
+	public ColorButtonListener(MainPanel mainPanel, Color c) {
+		this.mainPanel = mainPanel;
+		this.c = c;
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		mainPanel.setBackground(c);
+	}
 }
+
 
 public class Toolbar extends JToolBar {
 	private static final long serialVersionUID = 1L;
-	public Toolbar() {
-		// 툴바에 버튼을 붙임
-		add(new JButton("버튼1"));
-		add(new JButton("버튼2"));
+	
+	public Toolbar(MainPanel mainPanel) {
+		
+		final JButton redButton = new JButton("RED");
+		final JButton blueButton = new JButton("BLUE");
+		// 버튼에 이벤트를 연결('클릭' 이벤트)
+		redButton.addActionListener(new ColorButtonListener(mainPanel, Color.red));
+		blueButton.addActionListener(new ColorButtonListener(mainPanel, Color.blue));
+		
+		add(redButton);
+		add(blueButton);
 	}
 }
